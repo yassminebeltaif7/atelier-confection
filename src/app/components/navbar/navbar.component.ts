f8eb92688f1ce20641e28c38875df7473c39804b
yassmine beltaif <beltaifyasmine7@gmail.com>
2026-09-09 13:22:27 +0200

    Ajout Light/Dark Mode complet sur toute application avec variables CSS

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,11 +8,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isDarkMode = false;
+
+  ngOnInit(): void {
+    const saved = localStorage.getItem('darkMode');
+    this.isDarkMode = saved === 'true';
+    this.applyMode();
+  }
 
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', String(this.isDarkMode));
+    this.applyMode();
+  }
+
+  private applyMode() {
     document.body.classList.toggle('dark-mode', this.isDarkMode);
   }
 }
